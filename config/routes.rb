720645bb82c2f8c819fe4218bdc66605b38e4323
diana2341 <dianaponce2341@gmail.com>
2020-04-19 00:28:32 -0400

    fixed routes and session controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,20 @@
 Rails.application.routes.draw do
-  resources :users
+  root to: "listings#index"
+
+  resources :sessions, only: [:new, :create, :destroy]
+  get '/signup', to: 'users#new', as: 'signup'
+  get '/login', to: 'sessions#new', as: 'login'
+  get '/logout', to: 'sessions#destroy', as: 'logout'
   resources :trips
-  resources :reviews
-  resources :reservations
   resources :locations
-  resources :listings
+  resources :reviews
+  resources :reservations 
+  resources :listings ,only: [:destroy,:new,:create,:show,:index,:edit,:update]
+  
+  resources :listings do 
+    resources :reservations, only: [:new,:create,:show,:index]
+    resources :reviews, only: [:new,:create]
+  end
+  resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
